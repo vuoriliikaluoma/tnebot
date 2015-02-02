43e28851496aa826f6afbdd0739b339898929bf7
Kimmo Matias Vuori Liikaluoma <vuoriliikaluoma@gmail.com>
2015-02-02 13:08:10 +0100

    New License, Updated Readme & Optimized code in preparation for support of other social media.

--- README.md
+++ README.md
@@ -1,12 +1,21 @@
 tnebot
 ======
 
-The Nintendo Elements RSS Crawler and Reddit and Twitter poster bot.
+The Nintendo Elements RSS Crawler and Twitter poster bot.
+(Support for other social media is being worked on.)
 
-The project is not done and so far only parses RSS feeds but doesn't actually do anything.
+So far the Bot works well as a Twitter bot.
+It's currently used on the @NintendoElement Twitter account.
+We have the script set up to run every hour on the hour.
 
 
-How to set up:
+How to set up: (Twitter only at the moment.)
 
-1. Edit the urlList array to contain your RSS feeds.
-2. TODO...
+1. Go to http://apps.twitter.com with the account for the bot and create a new app with read/write permissions.
+2. Edit the urlList array to contain your RSS feeds.
+3. Set the twitter_api_key, twitter_api_key_secret, twitter_oauth_token and twitter_oauth_token_secret variables
+4. Schedule the script to run every so often. (See your system documentation on how to accomplish this.)
+
+And you're done!
+
+
--- tnebot.rb
+++ tnebot.rb
@@ -9,17 +9,34 @@ require 'sqlite3'
 #                TNEBot                #
 ########################################
 #
-# A simple RSS to Twitter and Reddit bot.
+# A simple RSS to Social Media bot.
 #
 # Written by: @VuoriLiikaluoma
 #
-# Version: 20140814
+# Version: 20150202
 #
 
 # Add your feeds here.
 urlList = [
   'http://example.com/rss'
 ]
+
+# Publish to Facebook?
+#facebook = false
+
+# Publish to Reddit?
+#reddit = false
+
+# Publish to Twitter?
+twitter = true
+twitter_api_key = "123456789ABCDEFGHIJKLMNOP"
+twitter_api_key_secret = "123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmno"
+twitter_oauth_token = "12345678-9ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmn"
+twitter_oauth_token_secret = "123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghij"
+
+################################################################################
+#        Do not touch the stuff below unless you know what you're doing.       #
+################################################################################
 
 # Open the database.
 db = SQLite3::Database.new "tnebot.sqlite3"
@@ -36,12 +53,6 @@ urlList.each do |url|
 end
 query = query[0..-6]+";"
 db.execute query
-
-# Publish to Reddit?
-reddit = false
-
-# Publish to Twitter?
-twitter = true
 
 ## RSS - Parse the RSS feeds.
 feeds = {}
@@ -64,9 +75,13 @@ urlList.each do |url|
   end
 end
 
-## Twitter - Prepare Twitters OAuth access token.
-def prepare_access_token(oauth_token, oauth_token_secret)
-  consumer = OAuth::Consumer.new("API key", "API secret",
+## Facebook - Prepare Facebook OAuth2 stuff.
+
+## Reddit - Prepare Reddit OAuth2 stuff.
+
+## Twitter - Prepare Twitter OAuth access token.
+def prepare_access_token(api_key, api_key_secret, oauth_token, oauth_token_secret)
+  consumer = OAuth::Consumer.new(api_key, api_key_secret,
     { :site => "https://api.twitter.com",
       :scheme => :header })
   # now create the access token object from passed values
@@ -74,28 +89,28 @@ def prepare_access_token(oauth_token, oauth_token_secret)
     :oauth_token => oauth_token,
     :oauth_token_secret => oauth_token_secret
   }
-  access_token = OAuth::AccessToken.from_hash(consumer, token_hash)
-  return access_token
+  oauth_access_token = OAuth::AccessToken.from_hash(consumer, token_hash)
+  return oauth_access_token
 end
-
-## Reddit - OAuth2 stuff.
-#TODO: Add OAuth2 code for Reddit...
 
 ## Output the feed items.
 feedTitles.each do |title|
   feeds[title].each do |item|
-    if twitter === true
-      twitter_access_token = prepare_access_token("Access token", "Access token secret")
-      if item.title.length > 108
-        tweet = "TNEBot: #{item.title[0..104]}... - #{item.link}"
-      else
-        tweet = "TNEBot: #{item.title} - #{item.link}"
-      end
-      twitter_update_hash = {'status' => tweet}
-      twitter_response = twitter_access_token.request(:post, 'https://api.twitter.com/1.1/statuses/update.json', twitter_update_hash, { 'Accept' => 'application/xml', 'Content-Type' => 'application/x-www-form-urlencoded' })
+    if facebook === true
+      #TODO: Add facebook support...
     end
     if reddit === true
       #TODO: Add reddit support...
+    end
+    if twitter === true
+      twitter_access_token = prepare_access_token(twitter_api_key, twitter_api_key_secret, twitter_oauth_token, twitter_oauth_token_secret)
+      if item.title.length > 116
+        tweet = "#{item.title[0..113]}... - #{item.link}"
+      else
+        tweet = "#{item.title} - #{item.link}"
+      end
+      twitter_update_hash = {'status' => tweet}
+      twitter_response = twitter_access_token.request(:post, 'https://api.twitter.com/1.1/statuses/update.json', twitter_update_hash, { 'Accept' => 'application/xml', 'Content-Type' => 'application/x-www-form-urlencoded' })
     end
   end
 end
